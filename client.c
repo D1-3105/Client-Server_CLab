ebb005968a63c682b59c79833b453546835fbfac
root <lyerhd@gmail.com>
2023-05-24 18:54:12 +0300

    Done

--- client.c
+++ client.c
@@ -1,1 +1,64 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
+
+struct request {
+	char *data;
+};
+
+struct response {
+	unsigned content_len;
+	unsigned status_code;
+	char *data;
+};
+
+int main()
+{
+    	int sock;
+    	struct sockaddr_in addr;
+
+    	sock = socket(AF_INET, SOCK_STREAM, 0);
+    	if(sock < 0) {
+        	perror("socket");
+        	exit(1);
+    	}
+
+    	addr.sin_family = AF_INET;
+    	addr.sin_port = htons(8080);
+    	addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+   	if(connect(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0){
+       		perror("connect");
+        	exit(2);
+	}
+	printf("Connected \n");
+	float a = 2123, b = 211, y = 37654;
+	float data[] = {a, b, y};
+	struct request on_send = { data };
+    	send(sock, on_send.data, sizeof(float)*3, 0);
+    	printf("Sended data \n");
+	struct response resp;
+	unsigned cl;
+	recv(sock, &cl, 4, 0);
+	printf("RECEIVED CONTENT LEN: %u\n", cl);
+	resp.content_len = cl;
+	printf("RECEIVED CONTENT\n");
+	recv(sock, &resp.status_code, sizeof(unsigned), 0);
+	//recv(sock, resp.data, cl-2*sizeof(unsigned), 0);
+	float fl_n=-1;
+	//printf("c: %f", fl_n);
+//printf("c: %x %f", resp.data, ((float *)resp.data)[0]);
+	if(resp.status_code == 200) {
+		//float answer = ((float*) resp.data)[0];
+		recv(sock, &fl_n, cl-2*sizeof(unsigned), 0);
+		printf("Answer: %f\n", fl_n);
+	}
+	else {
+
+		printf("Status: %i", resp.status_code);
+		printf("Detail: Division by zero");
+	}
+    	close(sock);
+
+	return 0;
+}
--- server.c
+++ server.c
@@ -1,61 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <malloc.h>
 
-struct response {
+typedef struct {
+	size_t content_len;
 	unsigned status_code;
 	char* data;
-};
+} response;
 
-struct request {
+typedef struct {
 	char* data;
-};
+} request;
 
 struct coefficients {
 	float a, b, y;
 };
 
-struct response do_work(struct request* buf) {
+response do_work(request* buf) {
 	if(buf->data != NULL) {
 		// buf->data = a, b, y
-		if (buf->data[0] == 0){
-			return {400, "Division by zero!"};
+		if (((float*) buf->data)[0] == 0){
+			response resp = {18+sizeof(unsigned)*2, 400, "Division by zero!"};
+			return resp;
 		}
-		struct coefficients coefs = buf->data;
-		float x = (coefs.y - coefs.b) / coefs.a;
-		return {200, *x};
+		struct coefficients coefs ={ ((float*) buf->data)[0], ((float*) buf->data)[1], ((float*) buf->data)[2] };
+		float *x = (float*) malloc(sizeof(float)*1);
+		*x = (coefs.y - coefs.b) / coefs.a;
+		response resp = {sizeof(float)+sizeof(unsigned)*2, 200, (char*)x};
+		return resp;
 	}
+	response resp = {sizeof(unsigned)*2, 500, NULL};
+	return resp;
 }
 
-void* receive_data() {
-	float buf[3];
+void* receive_data(int sock) {
+	float* buf = malloc(sizeof(float)*3);
 	int bytes_read;
-	while(1) {
-		bytes_read = recv(sock, buf, 3*sizeof(float), 0);
-		if(bytes_read == 0) {
-			return;
-		}
+	bytes_read = recv(sock, buf, 3*sizeof(float), 0);
+	if(bytes_read == 0) {
+		return;
 	}
+	printf("\ns:A: %f \n", buf[0]);
+	printf("\ns:B: %f \n", buf[1]);
+	printf("\ns:Y: %f \n", buf[2]);
 	return buf;
 }
 
-int respond(int s, struct response *resp, int len, int flags) {
-    int total = 0;
-    int n;
+int respond(int sock, response *resp, int len, int flags) {
+    	int total = 0;
+	int n;
+	printf("%u , %u \n", resp->status_code, resp->content_len);
+	printf("s:X: %f", *resp->data);
+	send(sock, &resp->content_len, sizeof(unsigned), 0);
+	send(sock, &resp->status_code, sizeof(unsigned), 0);
+	//if (resp->status_code == 200)
+	send(sock, resp->data, resp->content_len - sizeof(unsigned)*2, 0);
+	//for debug 
 
-    while(total < len)
-    {
-        n = send(s, buf+total, len-total, flags);
-        if(n == -1) { break; }
-        total += n;
-    }
+		//float test_n = 777;
+		//send(sock, &test_n, resp->content_len - sizeof(unsigned)*2, 0);
 
-    return (n==-1 ? -1 : total);
+
+
+//else
+		//send(sock, resp->data, resp->content_len - sizeof(unsigned)*2, 0);
+	return (n==-1 ? -1 : total);
 }
 
 void accept_ready(int listener){
 	int sock;
-	
 	while(1)
         {
                 sock = accept(listener, NULL, NULL);
@@ -63,9 +79,14 @@ void accept_ready(int listener){
                         perror("accept");
                         exit(3);
                 }
-		struct request received_data = { receive_data() };
-		struct response result = do_work();
-		send(sock, )
+		printf("\ns:ACCEPTED CONNECTION\n");
+		request received_request = { receive_data(sock) };
+		printf("\ns:ACCEPTED DATA\n");
+		response resp = do_work(&received_request);
+		printf("\ns:WORK DONE\n");
+		respond(sock, &resp, resp.content_len, 0);
+		printf("\ns:RESPONDED\n");
+		free(received_request.data);
 	}
 	close(sock);
 }
@@ -74,26 +95,24 @@ void serve_forever(){
 	int listener;
     	struct sockaddr_in addr;
     	int bytes_read;
-
     	listener = socket(AF_INET, SOCK_STREAM, 0);
     	if(listener < 0)
     	{
-        	perror("socket");
+        	perror("s:socket");
         	exit(1);
     	}
-    
+	printf("s:Listening socket...");
     	addr.sin_family = AF_INET;
-    	addr.sin_port = htons(3425);
+    	addr.sin_port = htons(8080);
     	addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    	if(bind(listener, (struct sockaddr *)&addr, sizeof(addr)) < 0)
-    	{
-        	perror("bind");
+    	if(bind(listener, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+        	perror("s:bind");
         	exit(2);
     	}
 
     	listen(listener, 1);
+	printf("\ns:BINDED 8080\n");
 	accept_ready(listener);
-    }
 }
 
 int main(){
